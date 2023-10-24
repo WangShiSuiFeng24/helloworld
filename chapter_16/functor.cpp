f43e93ebce121f5cc120082d585b9272804ef790
WangShiSuiFeng24 <andongming@mails.ccnu.edu.cn>
2023-10-24 16:51:07 +0800

    daily practice

---
+++ chapter_16/functor.cpp
@@ -0,0 +1,51 @@
+// functor.cpp -- using a functor
+#include <iostream>
+#include <list>
+#include <iterator>
+#include <algorithm>
+
+template <class T>      // functor class defines operator()()
+class TooBig
+{
+private:
+    T cutoff;
+public:
+    TooBig(const T & t) : cutoff(t) {}
+    bool operator()(const T &v) { return v > cutoff; }
+};
+
+void outint(int n) { std::cout << n << " "; }
+
+int main()
+{
+    using std::cout;
+    using std::endl;
+    using std::list;
+
+    using std::for_each;
+
+    TooBig<int> f100(100);
+
+    int vals[10] = {50, 100, 90, 180, 60, 210, 415, 88, 188, 201};
+    list<int> yadayada(vals, vals + 10);    // range constructor
+    list<int> etcetera(vals, vals + 10);
+
+    cout << "Original list:\n";
+    for_each(yadayada.begin(), yadayada.end(), outint);
+    cout << endl;
+
+    for_each(etcetera.begin(), etcetera.end(), outint);
+    cout << endl;
+
+    yadayada.remove_if(f100);
+    etcetera.remove_if(TooBig<int>(200));
+
+    cout << "Trimmed list:\n";
+    for_each(yadayada.begin(), yadayada.end(), outint);
+    cout << endl;
+
+    for_each(etcetera.begin(), etcetera.end(), outint);
+    cout << endl;
+
+    return 0;
+}
---
+++ chapter_16/strgst1.cpp
@@ -0,0 +1,27 @@
+// strgst1.cpp -- applying the STL to a string
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main()
+{
+    using namespace std;
+
+    string letters;
+
+    cout << "Enter the letter grouping (quit to quit): ";
+    while (cin >> letters && letters != "quit")
+    {
+        cout << "Permutation of " << letters << endl;
+        sort(letters.begin(), letters.end());
+        cout << letters << endl;
+
+        while (next_permutation(letters.begin(), letters.end()))
+            cout << letters << endl;
+
+        cout << "Enter next swquence (quit to quit): ";
+    }
+
+    cout << "Done.\n";
+    return 0;
+}
